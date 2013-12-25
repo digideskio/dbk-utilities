35269800380a2c936cc50b855c2fc0af490aa64f
Amir <amir.moravej@gmail.com>
2013-12-25 15:22:21 -0500

    Utility Functions

--- progress-counter/main.js
+++ progress-counter/main.js
@@ -13,21 +13,29 @@ function addProgressCounter() {
 			progresses.push(spans[i]);
 
 	for (var i = 0; i < progresses.length; ++i) {
+		
 		var upperBound = parseInt(progresses[i].innerHTML);
 		var duration = parseInt(progresses[i].getAttribute('data-duration'));
+		var from = parseInt(progresses[i].getAttribute('data-from'));
+		
 		if (isNaN(upperBound)) continue;
+
 		duration = isNaN(duration) ? 2300 : duration;
-		showCounter(progresses[i], upperBound, duration);
+		from = isNaN(from) ? 0 : from;
+
+		showCounter(progresses[i], from, upperBound, duration);
 	}
 };
 
-function showCounter(element, upperBound, duration) {
-	var interval = Math.round(duration / upperBound);
-	for (var i = 0; i <= upperBound; ++i) {
-		(function(i){
+function showCounter(element, from, upperBound, duration) {
+
+	var interval = Math.round(duration / (upperBound - from));
+
+	for (var i = from, j = 0; i <= upperBound; ++i, ++j) {
+		(function(i, j){
 			window.setTimeout(function(){
 				element.innerHTML = i;
-			}, i * interval);
-		}(i));
+			}, j * interval);
+		}(i, j));
 	};
 };
