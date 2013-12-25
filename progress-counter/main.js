d99ed59815da2d8ad044115b766726fd238efd07
Amir <amir.moravej@gmail.com>
2013-12-25 14:57:45 -0500

    Utility Functions

--- progress-counter/main.js
+++ progress-counter/main.js
@@ -5,21 +5,29 @@ function utils () {
 };
 
 function addProgressCounter() {
-	var progresses = docoment.getElementsByTagName('span').getElementsByClassName('counter');
+	var progresses = [],
+		spans = document.getElementsByTagName('span');
 
-	for (var i = 0; i < progresses; ++i) {
+	for (var i = 0; i < spans.length; ++i)
+		if (spans[i].getElementsByClassName('counter'))
+			progresses.push(spans[i]);
+
+	// console.log(spans);
+	// console.log(progresses);
+	for (var i = 0; i < progresses.length; ++i) {
 		var upperBound = parseInt(progresses[i].innerHTML);
+		console.log(upperBound)
 		if (isNaN(upperBound)) continue;
 		showCounter(progresses[i], upperBound);
 	}
 };
 
 function showCounter(element, upperBound) {
-	for (var i = 0; i < upperBound; ++i) {
+	for (var i = 0; i <= upperBound; ++i) {
 		(function(i){
 			window.setTimeout(function(){
 				element.innerHTML = i;
-			}, i * 500);
+			}, i * 100);
 		}(i));
 	};
 };
